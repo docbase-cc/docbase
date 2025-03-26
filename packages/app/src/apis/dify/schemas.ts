a5765c0289c967a352f9c74383606e2281ab8b9e
sovlookup <gonorth@qq.com>
2025-03-27 01:21:24 +0800

    添加 metadata_condition 类型

--- packages/app/src/apis/dify/schemas.ts
+++ packages/app/src/apis/dify/schemas.ts
@@ -8,6 +8,36 @@ export const DifyKnowledgeRequestSchema = z
       top_k: z.number().int(),
       score_threshold: z.number().min(0).max(1),
     }),
+    // 尚未支持 https://docs.dify.ai/zh-hans/guides/knowledge-base/external-knowledge-api-documentation#qing-qiu-ti-yuan-su
+    // metadata_condition: z
+    //   .object({
+    //     logical_operator: z.enum(["and", "or"]).default("and").optional(),
+    //     conditions: z.array(
+    //       z.object({
+    //         name: z.array(z.string()),
+    //         comparison_operator: z.enum([
+    //           "contains",
+    //           "not contains",
+    //           "start with",
+    //           "end with",
+    //           "is",
+    //           "is not",
+    //           "empty",
+    //           "not empty",
+    //           "=",
+    //           "≠",
+    //           ">",
+    //           "<",
+    //           "≥",
+    //           "≤",
+    //           "before",
+    //           "after",
+    //         ]),
+    //         value: z.string(),
+    //       })
+    //     ),
+    //   })
+    //   .optional(),
   })
   .openapi("DifyKnowledgeRequestSchema");
 
