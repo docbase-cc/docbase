2749d6270581cd09db12d1ba4f16a18e27155db1
sovlookup <gonorth@qq.com>
2025-03-27 01:44:53 +0800

    todo

--- packages/app/src/apis/base/embedder/index.ts
+++ packages/app/src/apis/base/embedder/index.ts
@@ -1,1 +1,6 @@
-// 配置 embedder 的 api
+// 管理知识库 embedder 的 API
+
+// docBase.getEmbedders;
+// docBase.resetEmbedders;
+// docBase.updateEmbedder;
+// docBase.unLoadDocLoader;
---
+++ packages/app/src/apis/base/manage/index.ts
@@ -0,0 +1,5 @@
+// 管理 知识库 的 API
+
+// docBase.addBase;
+// docBase.delBase;
+// docBase.getBase;
