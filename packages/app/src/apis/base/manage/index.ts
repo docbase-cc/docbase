82c589d4d55503e7be996faa45d6b9166dfb7c5a
sovlookup <gonorth@qq.com>
2025-03-27 19:14:33 +0800

    src

--- packages/app/src/apis/base/manage/index.ts
+++ packages/app/src/apis/base/manage/index.ts
@@ -1,5 +1,49 @@
 // 管理 知识库 的 API
+import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
 
-// docBase.addBase;
-// docBase.delBase;
-// docBase.getBase;
+const app = new OpenAPIHono();
+
+// Search
+const addBase = createRoute({
+  tags: ["manage"],
+  method: "put",
+  path: "/:knowledgeId/base",
+  summary: "add knowledge base",
+  security: [
+    {
+      Bearer: [],
+    },
+  ],
+  request: {
+    params: z.object({ knowledgeId: z.string() }),
+    body: {
+      content: {
+        "application/json": {
+          schema: z.object({}),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: z.object({}),
+        },
+      },
+      description: "Search results",
+    },
+  },
+});
+
+// 搜索
+app.openapi(addBase, async (c) => {
+  const docBase = c.get("docbase");
+  docBase.addBase;
+  docBase.delBase;
+  docBase.getBase;
+
+  // return c.json(res, 200);
+});
+
+export default app;
