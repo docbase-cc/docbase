768e03dcef5738e84bbf37637485c8fc0a22bbe0
sovlookup <gonorth@qq.com>
2025-04-01 13:13:11 +0800

    start

--- packages/ui/nuxt.config.ts
+++ packages/ui/nuxt.config.ts
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'static'
+    preset: "static",
   },
-  compatibilityDate: '2025-03-17',
+  compatibilityDate: "2025-03-17",
   devtools: { enabled: true },
   modules: [
-    '@unocss/nuxt',
-    '@pinia/nuxt',
-    'pinia-plugin-persistedstate/nuxt',
-    '@nuxt/icon',
+    "@unocss/nuxt",
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+    "@nuxt/icon",
   ],
-})
+});
