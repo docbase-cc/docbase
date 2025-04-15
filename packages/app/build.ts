586d8cd13bfcdc5eeb1935f45ca6a0e432f493db
sovlookup <gonorth@qq.com>
2025-04-15 20:57:57 +0800

    0.4.22

--- packages/app/build.ts
+++ packages/app/build.ts
@@ -1,4 +1,15 @@
+import { spawnSync } from "child_process";
+import { join } from "path";
+import { _dirname } from "./src/utils";
 import { copy } from "fs-extra";
+
+spawnSync("bun", ["x", "prisma", "generate"], {
+  stdio: "inherit",
+  env: {
+    DATABASE_URL: `file:${join(_dirname, ".docbase/data/db.sqlite")}`,
+    PRISMA_ENGINES_MIRROR: "https://registry.npmmirror.com/-/binary/prisma",
+  },
+});
 
 // 并行构建
 await Promise.all([
--- packages/app/dev.ts
+++ packages/app/dev.ts
@@ -3,6 +3,14 @@ import { join } from "path";
 import { version } from "~/package.json";
 import { _dirname } from "./src/utils";
 
+spawnSync("bun", ["x", "prisma", "generate"], {
+  stdio: "inherit",
+  env: {
+    DATABASE_URL: `file:${join(_dirname, ".docbase/data/db.sqlite")}`,
+    PRISMA_ENGINES_MIRROR: "https://registry.npmmirror.com/-/binary/prisma",
+  },
+});
+
 spawnSync(
   "bun",
   ["x", "prisma", "migrate", "dev", "--skip-generate", "-n", version],
