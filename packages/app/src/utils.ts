1590594681dc1f5fc095fd86c84b66880904b8c0
sovlookup <gonorth@qq.com>
2025-04-16 21:18:59 +0800

    dirname

--- packages/app/src/utils.ts
+++ packages/app/src/utils.ts
@@ -4,16 +4,22 @@ import path, { join } from "path";
 import { execPath } from "process";
 import { fileURLToPath } from "url";
 
-let _dirname = path.dirname(fileURLToPath(import.meta.url));
+const dirname = () => {
+  let _dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// 如果是 bun 编译后的环境，需要使用 execPath
-if (_dirname.includes("~BUN")) {
-  _dirname = path.dirname(execPath);
-}
+  // 如果是 bun 编译后的环境，需要使用 execPath
+  if (_dirname.includes("~BUN")) {
+    _dirname = path.dirname(execPath);
+  }
 
-__dirname = _dirname;
-console.debug("[_dirname] ", _dirname);
+  globalThis.__dirname = _dirname;
 
+  console.debug("[__dirname] ", __dirname);
+
+  return _dirname;
+};
+
+export const _dirname = dirname();
 export const prodPublicPath = join(_dirname, "public");
 export const prodPublicExists = await exists(prodPublicPath);
 export const _binDufs = join(
@@ -22,5 +28,3 @@ export const _binDufs = join(
   platform() === "win32" ? "dufs.exe" : "dufs"
 );
 export const _binDufsExists = await exists(_binDufs);
-
-export { _dirname };
